f77fdb85392aa51ab45c397f92b3e98a4fac9e74
sammy <chensongsen03@gmail.com>
2023-08-02 21:59:33 -0500

    changed for to htmlFor

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import Image from 'next/image'
-import CustomButton from './CustomButton'
 
 const Hero = () => {
 
@@ -16,7 +15,7 @@ const Hero = () => {
                     <p className="mb-8 leading-relaxed">We offer a multitude of AI services. Send us your email and we'll respond with a free AI audit of your business</p>
                     <div className="flex w-full md:justify-start justify-center items-end">
                         <div className="relative mr-4 lg:w-full xl:w-1/2 w-2/4">
-                            <label for="hero-field" className="leading-7 text-sm text-gray-600">Yes! I want to save time on my business</label>
+                            <label htmlFor="hero-field" className="leading-7 text-sm text-gray-600">Yes! I want to save time on my business</label>
                             <input type="text" id="hero-field" name="hero-field" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:ring-indigo-200 focus:bg-transparent focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                         </div>
                         <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Schedule a Call</button>
